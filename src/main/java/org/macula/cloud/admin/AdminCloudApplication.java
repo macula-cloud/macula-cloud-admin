293ee99ab975133cb1d95a071968c6b4dbe78de6
Jokeway <jokeway@hotmail.com>
2020-08-03 14:35:59 +0800

    [IMP remove unused code

--- src/main/java/org/macula/cloud/admin/AdminCloudApplication.java
+++ src/main/java/org/macula/cloud/admin/AdminCloudApplication.java
@@ -3,15 +3,11 @@ package org.macula.cloud.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-
 @SpringCloudApplication
-@EnableAdminServer
 public class AdminCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminCloudApplication.class, args);
 	}
-	
 
 }
